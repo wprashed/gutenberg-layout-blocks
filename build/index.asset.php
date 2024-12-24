301c9dd6e6db62392eb2b8ab189fe7a8b3e655b8
Md Rashed Hossain <wprashed@icloud.com>
2024-12-24 13:37:48 +0600

    Update index.asset.php

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c099baa40e989230092a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'ac33556bdaf6ef9e0ab7');
